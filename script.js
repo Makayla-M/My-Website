fbf3efc66f9bc97ef61625af630a3d057a528e63
Makayla-M <makaylajmcvea@gmail.com>
2022-06-02 16:31:55 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+  document.body.style.backgroundColor = "#5CDB95";
+
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+  document.body.style.backgroundColor = "#5CDB95";
+}
+ 
+ function openhome(){
+ window.location.href = "file:///Users/makaylamcvea/Downloads/homepage.html"
+			
+}
+
+function openpai(){
+ window.location.href = "file:///Users/makaylamcvea/Downloads/projects.html"
+			
+}
+function openabout(){
+ window.location.href = "file:///Users/makaylamcvea/Downloads/about.html"
+			
+}
+function cbutton(){
+		alert("Mathematical Logic: The Foundations of Mathematics, Philosophy, and Computer Science \nDr Griffiths Cambridge | Faculty of Philosophy \nMathematical logic is the formalized study of the basic principles of reasoning and rational thought. In this course, you will attain fluency in multiple logical systems and we will explore foundational issues in mathematics, philosophy, and computer science.");
+	}
+function sbutton(){
+	alert("What fills the space between stars in a galaxy? The tenuous gas and dust that fills this space is referred to as the interstellar medium (ISM). This project focuses on understanding the ionized gas component of the ISM in the disks of the nearby Andromeda (M31) and Triangulum (M33) galaxies. The SIP interns will work with multi slit spectra obtained by the mentoring team using the DEIMOS instrument on the Keck II 10-m telescope. The interns will use Python spectroscopic data analysis techniques to detect and carry out a detailed characterization of the spectral emission lines associated with the ionized gas ISM component of these two galaxies, and draw conclusions about its rotational dynamics, chemical composition, and physical properties.")
+}
+
+
+function openrhino(){
+	window.location.href= "file:///Users/makaylamcvea/Downloads/rhino.html";
+}
+
+
+
+
